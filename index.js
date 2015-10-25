df31670bbe64bc82ecb32daf202a7241c28e214f
Allison Stafford <you@allie3@rocketmail.com>
2015-10-24 19:52:01 -0700

    changed pic names, added transition for background and changed hover from css to jscript for more flexibility

--- index.js
+++ index.js
@@ -1,39 +1,71 @@
-function mouseenter() { 
-	var parent = document.getElementsByClassName("parent");
-	for (var i = 0; i < parent.length; i++) {
-		parent[i].style.background = "grey";
-	} 
-	var bod = document.getElementById("body1");
-	bod.style.background = "grey";
+function picchange(e) { 
+	var e = e || window.event;
+	var target = e.target || e.srcElement; 
+	var classes = target.className.split(" ");
+	if (classes) {
+		for (var i = 0; i < classes.length; i++) { 
+			if (classes[i] === "logos") { 
+				for (var i = 0; i < logos.length; i++) {
+					if (logos[i] === target) {
+						target.style.content = "url(pic" + (i + 1) + ".jpg";
+						target.style.cursor =  "pointer";
+						parent1.style.background = "#4C4C4C";
+						body.style.background = "#4C4C4C";
+						parent2.style.background = "#4C4C4C";
+					};
+				}; 
+			};
+		};
+	};
 
-} 
-
-function mouseout() {
-	var y = document.getElementById("body1");
-	var z = document.getElementsByClassName("parent");
-	y.style.background = "#907D7C";
-	for (var i = 0; i < z.length; i++) {
-		z[i].style.background = "#907D7C";
-	}
-}
-
-function findevent(e) {
+ };
+  
+function mouseout(e) {
 	var e = e || window.event;
 	var target = e.target || e.srcElement;
+	var classes = target.className.split(" ");
+	if (classes) {
+		for (var i = 0; i < classes.length; i++) {
+			if (classes[i] === "logos") {
+				for (var i = 0; i < logos.length; i++) {
+					if (logos[i] === target ) {
+						target.style.content = "url(img" + (i + 1) + ".png";
+						parent1.style.background = "#907D7C";
+						body.style.background = "#907D7C";
+						parent2.style.background = "#907D7C";
+					}
+				};
+			};
+		};
+	};
+};
+
+function excludealt(e) {
+	var e = e || window.event;
+	var target = e.target || e.srcElement;
+	var parent = document.getElementsByClassName("parentz");
+	var classes = target.className.split(" ");
 		for (var i = 0; i < logos.length; i++) {
-			if (target !== logos[i]) {
+			if (target !== logos[i] && target !== parent[i]) { //need logic to exclude non logos
 			logos[i].style.display = "none";
+			console.log("excludealt");
+		};
+	};
+	if (classes) {
+		for (var i = 0; i < classes.length; i++) { 
+			if (classes[i] === "logos") { 
+				for (var i = 0; i < logos.length; i++) {
+					if (logos[i] === target) {
+						target.style.content = "url(pic" + (i + 1) + ".jpg";
+						console.log("this works");
+					}
+				}
+			}
 		}
-
 	}
-}
+};
 
-var logos = document.getElementsByClassName("logos");
-for (var i = 0; i < logos.length; i++) {
-	logos[i].addEventListener('mouseenter', mouseenter);
-	logos[i].addEventListener('mouseout', mouseout);
-}
-
-
-var parent = document.getElementById("parent");
-parent.addEventListener("click", findevent);
+var logos = document.getElementsByClassName("logos"); 
+body.addEventListener("mouseover", picchange);
+body.addEventListener("mouseout", mouseout);
+body.addEventListener("click", excludealt); 
